35b06896ff1c022ae6ae1b335aea8024069fd601
/Oꜰꜰʟɪɴᴇ ‌[Aꜰᴋ] <93505687+LOG-AFK-xD@users.noreply.github.com>
2022-02-03 11:18:23 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 echo "Cloning Repo, Please Wait..."
-git clone https://github.com/log-afk-xd/Radio.git /Radio
+git clone https://github.com/AsmSafone/RadioPlayerV3.git /RadioPlayerV3
 echo "Installing Requirements..."
-cd /Radio
+cd /RadioPlayerV3
 pip3 install -U -r requirements.txt
 echo "Starting Bot, Please Wait..."
 python3 main.py
